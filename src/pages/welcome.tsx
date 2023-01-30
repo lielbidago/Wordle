45a516a04297ed35f4fa84d3e2d0394096558c4f
lielbidago <liloplev@gmail.com>
2023-01-30 16:57:39 +0200

    added tests

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 import './welcome.scss';
 
+
 export function WelcomePage(){
     
     const UserName = localStorage.getItem('UserName');
@@ -8,16 +9,11 @@ export function WelcomePage(){
     
     return (
         <div className="welcomePage">
-            <div className="welcome-header">
-            
-            </div>
             <div className="welcome-main">
                 <h1>hello {user}!</h1>
                 <h4>welcome to my</h4>
-                {/* <br></br> */}
                 <div className="wordle-icon leRotateYZoomIn"><h1>Wordle</h1></div>
                 <h4>game</h4>
-                {/* <br></br> */}
                 <Link role="button" type="button" className=" start btn btn-info" to={`/wordle`}>START</Link>
             </div>
             
